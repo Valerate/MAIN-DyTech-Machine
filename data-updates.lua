330bb4750af70b5137152ad140eaa94f35dcf0fc
Valerate <kassyesp@hotmail.com>
2016-08-06 21:11:55 +0200

    Fixes

--- data-updates.lua
+++ data-updates.lua
@@ -1,2 +1,8 @@
 require "config"
 
+if Config.Tools then
+require("prototypes.tools.item")
+require("prototypes.tools.recipe")
+require("prototypes.tools.tech")
+
+end
--- data.lua
+++ data.lua
@@ -206,12 +206,7 @@ require("prototypes.chests.logistic-chest-tech")
 end
 
 
-if Config.Tools then
-require("prototypes.tools.item")
-require("prototypes.tools.recipe")
---require("prototypes.tools.tech")
 
-end
 
 --[[ Beacons Check ]]--
 if Config.Beacons then
